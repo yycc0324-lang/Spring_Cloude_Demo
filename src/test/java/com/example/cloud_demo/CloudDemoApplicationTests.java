bd2dbeb11fdc473a7257e812cb923b6d7d7216db
伊橙 <a7@MacBook-Air.local>
2026-03-27 13:22:00 +0800

    微服务分布式demo

---
+++ src/test/java/com/example/cloud_demo/CloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
